0315030d0ff5b2af622970ef0977f5317db92728
viktorx64 <viktor.sundqvist2@gmail.com>
2020-09-28 16:42:32 +0200

    nu funkar det

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 //app.use('/static', express.static('public'));
 
 app.get('/', function(req, res){
-   res.sendFile(__dirname + "/pages/index.html");
+   // res.sendFile(__dirname + "/pages/index.html"); // deprecated
+   res.sendFile(__dirname + "/pages/hello.html");
 });
 app.get('/home', function(req, res){
     res.sendFile(__dirname + "/pages/hello.html");
  });
  app.get('/about', function(req, res){
-    res.sendFile(__dirname + "/pages/about.html");
+    res.sendFile(__dirname + "/pages/about.htm");
  });
 
 app.listen(process.env.PORT || 3000);
