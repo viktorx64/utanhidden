5a70c874d3be601456bd4ebb2384545f5d6f3889
viktorx64 <viktor.sundqvist2@gmail.com>
2020-09-28 17:07:53 +0200

    some commitment

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//app.use('/static', express.static('public'));
-
 app.get('/', function(req, res){
-   // res.sendFile(__dirname + "/pages/index.html"); // deprecated
    res.sendFile(__dirname + "/pages/hello.html");
 });
 app.get('/home', function(req, res){
@@ -13,6 +10,9 @@ app.get('/home', function(req, res){
  });
  app.get('/about', function(req, res){
     res.sendFile(__dirname + "/pages/about.htm");
+ });
+ app.get('*', function(req, res){
+    res.sendFile(__dirname + "/pages/index.html");
  });
 
 app.listen(process.env.PORT || 3000);
